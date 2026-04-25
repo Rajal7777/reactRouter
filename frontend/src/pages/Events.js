3dec59c2633f63cbf5f43d9c1256e5d1273a3aa9
Rajal7777 <suwalrajal57@gmail.com>
2026-04-25 10:08:58 +0900

    fetch data and display

---
+++ frontend/src/pages/Events.js
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+
+import EventsList from "../components/EventsList";
+
+function EventsPage() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [fetchedEvents, setFetchEvents] = useState();
+  const [error, setError] = useState();
+
+  useEffect(() => {
+    async function fetchEvents() {
+      setIsLoading(true);
+      const response = await fetch("http://localhost:8080/events");
+
+      if (!response.ok) {
+        setError("Fetching events failed.");
+      } else {
+        const resData = await response.json();
+        setFetchEvents(resData.events);
+      }
+      setIsLoading(false);
+    }
+    fetchEvents();
+  }, []);
+
+  return(
+    <>
+    <div style={{ textAlign: 'center'}}>
+      {isLoading && <p>Loading ...</p> }
+      {error && <p>{error}</p> }
+    </div>
+    {!isLoading && fetchedEvents && <EventsList events={fetchedEvents} />}
+    </>
+  )
+}
+
+export default EventsPage;
